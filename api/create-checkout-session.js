bd2b915c35e071072e108c5eacf4ad96b7cc8adc
blockjke <102760642+blockjke@users.noreply.github.com>
2025-07-25 15:58:30 +0300

    Create create-checkout-session.js

---
+++ api/create-checkout-session.js
@@ -0,0 +1,20 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price: 'your_price_id', // Замените на ваш Price ID
+      quantity: 1,
+    }],
+    mode: 'subscription',
+    success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${req.headers.origin}/canceled`,
+  });
+
+  res.json({ id: session.id });
+}
